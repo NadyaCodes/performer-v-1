152f352d390b711a3d8c1d02723d3a88cc4ee40c
NadyaCodes <nadyacorscadden@gmail.com>
2023-07-20 00:28:08 -0400

    search and find location object

--- src/components/AddProgram/AddProgramComponent.tsx
+++ src/components/AddProgram/AddProgramComponent.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import ProgramForm from "./ProgramForm";
+import { api } from "@component/utils/api";
+import { LocationObject } from "../ProgramSearch/types";
 
 export default function AddProgramComponent() {
+  const utils = api.useContext();
+
+  const findLocation = async (cityProv: LocationObject) => {
+    const prismaLocation = await utils.location.getOne.fetch(cityProv);
+    return prismaLocation;
+  };
   return (
     <div className="m-6 flex flex-col items-center justify-center">
       <h1 className="mb-2 text-4xl font-bold text-gray-900">
         Enter Program Details
       </h1>
-      <ProgramForm />
+      <ProgramForm findLocation={findLocation} />
     </div>
   );
 }
--- src/components/AddProgram/ProgramForm.tsx
+++ src/components/AddProgram/ProgramForm.tsx
@@ -1,19 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
+import pkg, { School, Location, SchoolLocation } from "@prisma/client";
+import { api } from "@component/utils/api";
 
 import { sanitize, isSupported } from "isomorphic-dompurify";
 import DOMPurify from "isomorphic-dompurify";
 
+import { PrismaClient } from "@prisma/client";
+// import { PrismaClient, Prisma } from "@prisma/client";
+
+// const prisma = new PrismaClient();
+
 import {
   DisciplineObject,
   FormErrorObject,
+  LocationObject,
   NewProgramSubmission,
   TypeObject,
 } from "../ProgramSearch/types";
 import Form from "./Form";
+import SubmissionComplete from "./SubmissionComplete";
 
-export default function ProgramForm() {
+interface ProgramFormProps {
+  findLocation: (cityProv: LocationObject) => Promise<Location | null>;
+}
+
+export default function ProgramForm({ findLocation }: ProgramFormProps) {
+  const utils = api.useContext();
   let firstId = uuidv4();
+  const [submissionComplete, setSubmissionComplete] = useState(false);
+
   const blankSchool = {
     schoolName: "",
     city: "",
@@ -116,6 +132,202 @@ export default function ProgramForm() {
     return errorsPresent ? formErrorObject : false;
   };
 
+  const findLocation2 = async (cityProv: LocationObject) => {
+    const prismaLocation = await utils.location.getOne.fetch(cityProv);
+    return prismaLocation;
+  };
+  // const prisma = new PrismaClient();
+
+  // const submitSchool = async () => {
+  //   return <LocationDataFetcher formData={formData[0]} />;
+  // };
+  // const submitSchool = async (formData: NewProgramSubmission) => {
+  //   return <LocationDataFetcher formData={formData} />;
+  // };
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     if (formData[0]) {
+  //       const locationData = {
+  //         city: formData[0]?.city.toLowerCase(),
+  //         province: formData[0]?.province.toLowerCase(),
+  //       };
+  //       const { data: prismaLocation } = await api.location.getOne.useQuery(
+  //         locationData
+  //       );
+  //       console.log(prismaLocation);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, [formData]);
+
+  // const [formData2, setFormData2] = useState<LocationObject>({
+  //   city: "toronto",
+  //   province: "ontario",
+  // });
+
+  // const submitSchool2 = async () => {
+  //   return <LocationDataFetcher formData={formData2} />;
+  // };
+  //   for (const dataObject of formData) {
+  //     const locationData = {
+  //       city: dataObject.city.toLowerCase(),
+  //       province: dataObject.province.toLowerCase(),
+  //     };
+
+  //     // Fetch or create the location in the database
+  //     let prismaLocation = await prisma.location.findFirst({
+  //       where: {
+  //         city: {
+  //           equals: locationData.city,
+  //         },
+  //         province: {
+  //           equals: locationData.province,
+  //         },
+  //       },
+  //     });
+
+  //     if (!prismaLocation) {
+  //       prismaLocation = await prisma.location.create({
+  //         data: locationData,
+  //       });
+  //     }
+  //     console.log(prismaLocation);
+  //   }
+  // };
+
+  // Call the function with your formData array
+  // const formDataCopy = [...formData]; // Your NewProgramSubmission array
+  // submitSchool(formDataCopy)
+  //   .then(() => {
+  //     console.log("Schools submitted to the database successfully!");
+  //     prisma.$disconnect(); // Disconnect Prisma client after the operation is done
+  //   })
+  //   .catch((error) => {
+  //     console.error("Error submitting schools:", error);
+  //     prisma.$disconnect(); // Disconnect Prisma client on error as well
+  //   });
+
+  // const submitSchool = async (formData: NewProgramSubmission[]) => {
+
+  // const { data: locationData } = api.example.getAll.useQuery();
+
+  // const fetchLocation = async (dataObject: LocationObject) => {
+  //   // Use the locationData here if needed
+  //   console.log(locationData);
+  // };
+
+  // formData.forEach((dataObject) => {
+  //   const locationData = {
+  //     city: dataObject.city.toLowerCase(),
+  //     province: dataObject.province.toLowerCase(),
+  //   };
+  //   fetchLocation(locationData);
+  // });
+  // const fetchLocation = async (dataObject: LocationObject) => {
+
+  //   const { data: locationData } = api.example.getAll.useQuery();
+  //   console.log(locationData);
+  // };
+  // formData.forEach((dataObject) => {
+  //   const locationData = {
+  //     city: dataObject.city.toLowerCase(),
+  //     province: dataObject.province.toLowerCase(),
+  //   };
+  //   fetchLocation(locationData);
+  // });
+
+  // const addExample = () => {
+  //   createExample({
+  //     text: "hello",
+  //   });
+  // };
+  // };
+  // const fetchLocation = async (dataObject: NewProgramSubmission) => {
+  //   const locationData = {
+  //     city: dataObject.city.toLowerCase(),
+  //     province: dataObject.province.toLowerCase(),
+  //   };
+  //   const { data: prismaLocation, isLoading } =
+  //     useLocationQuery(locationData);
+  //   console.log("prismaLocation: ", prismaLocation);
+  // };
+  // const fetchLocation = async (dataObject: NewProgramSubmission) => {
+  //   const locationData = {city: dataObject.city, province: dataObject.province}
+  //   const allLocations = api.location.getOne(locationData)
+  //   // const { data, isLoading, error } = useQuery('getAllLocations', getAll);
+
+  //   // return api.location.getAll({
+  //   //   where: {
+  //   //     city: {
+  //   //       equals: dataObject?.city,
+  //   //     },
+  //   //     province: {
+  //   //       equals: dataObject?.province,
+  //   //     },
+  //   //   },
+  //   // });
+  // };
+
+  // const addLocation = async (formData: NewProgramSubmission) => {
+  //   const locationObject = {
+  //     city: formData.city.toLowerCase(),
+  //     province: formData.province.toLowerCase(),
+  //   };
+  //   const { mutate: createLocation } = api.location.add.useMutation({
+  //     async onSuccess() {
+  //       await utils.location.getAll.invalidate();
+  //     },
+  //     onError(error) {
+  //       console.log("createSchool error: ", error);
+  //     },
+  //   });
+
+  //   createLocation(locationObject);
+  // };
+
+  // const fetchSchool = async (dataObject: NewProgramSubmission) => {
+  //   return prisma.school.findFirst({
+  //     where: {
+  //       name: {
+  //         equals: dataObject?.schoolName,
+  //       },
+  //     },
+  //   });
+  // };
+
+  // const fetchSchoolLocation = async (location: Location, school: School) => {
+  //   return prisma.schoolLocation.findFirst({
+  //     where: {
+  //       schoolId: {
+  //         equals: school.id,
+  //       },
+  //       locationId: {
+  //         equals: location.id,
+  //       },
+  //     },
+  //   });
+  // };
+
+  // for (const dataObject of formData) {
+  //   let prismaLocation =
+  //     (await fetchLocation(dataObject)) || (await addLocation(dataObject));
+  //   console.log(prismaLocation);
+  //   // let prismaSchool = await fetchSchool(dataObject);
+  //   // let prismaSchoolLocation =
+  //   //   prismaLocation &&
+  //   //   prismaSchool &&
+  //   //   (await fetchSchoolLocation(prismaLocation, prismaSchool));
+  // }
+  // };
+
+  const findLoc = async (locationObject: LocationObject) => {
+    const prismaLocation = await findLocation2(locationObject);
+    return prismaLocation;
+    console.log("prismaLocation", prismaLocation);
+  };
+
   const submitForm = () => {
     const errorsArray: FormErrorObject[] = [];
     let safeToSubmit = true;
@@ -128,6 +340,24 @@ export default function ProgramForm() {
     });
     setFormErrors(errorsArray);
     if (safeToSubmit) {
+      setSubmissionComplete(true);
+      // setTimeout(() => {
+      //   setSubmissionComplete(false)
+      // })
+      // formData.forEach((schoolObject) => {
+      //   const locationObject = {
+      //     city: schoolObject.city,
+      //     province: schoolObject.province,
+      //   };
+      //   const prismaLocation = findLoc(locationObject);
+      //   console.log("prismaLocation", prismaLocation);
+      //   if (!prismaLocation) {
+      //     // console.log("hey");
+      //   } // submitSchool(schoolObject);
+      // });
+      // formData.forEach((dataObject) => {
+      //   submitSchool(dataObject)
+      // })
     }
 
     console.log("safeToSubmit: ", safeToSubmit);
@@ -139,7 +369,9 @@ export default function ProgramForm() {
 
   return (
     <div className="flex flex-col content-center justify-center">
+      {submissionComplete && <SubmissionComplete formData={formData} />}
       <div className="flex flex-col border-2">
+        {/* {locationData && locationData.city} */}
         {formDisplay}
         <div className="flex justify-center">
           <button
