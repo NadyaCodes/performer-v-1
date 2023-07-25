c91ba6cf38a8e518725309ecdc0917273ee9dce3
NadyaCodes <nadyacorscadden@gmail.com>
2023-07-25 16:43:00 -0400

    fetch city info and create province routes on server

---
+++ src/components/ProgramDisplay/SelectNext.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Picker from "@component/pages/Picker";
+import Link from "next/link";
+
+interface SelectNextProps {
+  selectNextOptions: {
+    style: string;
+    discipline?: string;
+    province?: string;
+    link: string;
+    backLink: string;
+    nextValue: string;
+  };
+  buttonList: string[];
+}
+
+const SelectNext: React.FC<SelectNextProps> = ({
+  selectNextOptions,
+  buttonList,
+}) => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <h1 className="text-5xl font-extrabold capitalize tracking-tight text-white sm:text-[5rem]">
+        Which {selectNextOptions.nextValue}?
+      </h1>
+      <div
+        style={{
+          display: "flex",
+          margin: "2rem",
+        }}
+        className="rounded border-2 border-green-300"
+      >
+        <Picker
+          buttonOptions={buttonList}
+          currentLink={selectNextOptions.link}
+          last={true}
+        />
+      </div>
+      <Link href={selectNextOptions.backLink} className="p-2">
+        <button className="rounded border border-blue-500 bg-transparent px-4 py-2 font-semibold text-white hover:border-transparent hover:bg-blue-500 hover:text-white">
+          Back
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default SelectNext;
