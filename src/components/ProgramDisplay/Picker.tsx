60b9ab3b1217b8e1e96024face75a20de093eb3e
NadyaCodes <nadyacorscadden@gmail.com>
2023-07-27 13:04:25 -0400

    display readable values in course selector

--- src/components/ProgramDisplay/Picker.tsx
+++ src/components/ProgramDisplay/Picker.tsx
@@ -1,3 +1,8 @@
+import {
+  disciplinesFull,
+  provincesFull,
+  stylesFull,
+} from "@component/data/constants";
 import { type NextPage } from "next";
 
 import Link from "next/link";
@@ -13,6 +18,10 @@ const Picker: NextPage<PickerProps> = ({
   last,
 }) => {
   const buttonDisplay = buttonOptions.map((option) => {
+    const stylesKeys = Object.keys(stylesFull);
+    const disciplinesKeys = Object.keys(disciplinesFull);
+    const provincesKeys = Object.keys(provincesFull);
+    let buttonText = option;
     const buttonName = `${option}_button`;
     let link = currentLink + "/" + option;
 
@@ -20,10 +29,22 @@ const Picker: NextPage<PickerProps> = ({
       link += "/select-next";
     }
 
+    if (stylesKeys.includes(option)) {
+      buttonText = stylesFull[option] || option;
+    }
+
+    if (disciplinesKeys.includes(option)) {
+      buttonText = disciplinesFull[option] || option;
+    }
+
+    if (provincesKeys.includes(option)) {
+      buttonText = provincesFull[option] || option;
+    }
+
     return (
       <Link href={link} key={buttonName} className="p-2">
-        <button className="rounded border border-blue-500 bg-transparent px-4 py-2 font-semibold text-white hover:border-transparent hover:bg-blue-500 hover:text-white">
-          {option}
+        <button className="rounded border border-blue-500 bg-transparent px-4 py-2 font-semibold capitalize text-white hover:border-transparent hover:bg-blue-500 hover:text-white">
+          {buttonText}
         </button>
       </Link>
     );
