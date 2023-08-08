253f64c49d6f5ab0361fddb34d607fd5e4d88d35
NadyaCodes <nadyacorscadden@gmail.com>
2023-08-08 09:48:26 -0400

    conditionally hide the search bar

--- src/components/ProgramSearch/Search.tsx
+++ src/components/ProgramSearch/Search.tsx
@@ -17,35 +17,43 @@ export default function Search() {
 
   return (
     <div className="z-20 mb-3 mt-7 flex justify-around">
-      <input
-        className="focus:shadow-outline w-96 max-w-6xl appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow-md focus:outline-none"
-        id="username"
-        type="text"
-        placeholder="Search"
-        value={(filterContext && filterContext.searchTerm) || ""}
-        onChange={(e) =>
-          filterContext && filterContext.setSearchTerm(e.target.value)
-        }
-      />
-      <button
-        className="mx-5 flex items-center rounded px-3 outline outline-cyan-700 hover:scale-105 hover:text-indigo-200 hover:outline-indigo-200"
-        style={{ boxShadow: "none" }}
-        onClick={() => setSearchResults(filterContext?.searchTerm || "")}
-      >
-        <span className="p-1">Search</span>
-        <span className="p-1">{search}</span>
-      </button>
-      <button
-        className="mx-1 flex items-center justify-center rounded px-3 outline outline-pink-400 hover:scale-105 hover:text-indigo-200 hover:outline-indigo-200"
-        style={{ boxShadow: "none" }}
-        onClick={() => {
-          setSearchResults("");
-          filterContext?.setSearchTerm("");
-        }}
-      >
-        <span className="p-1">Clear Search Term</span>
-        <span className="p-1">{xMark}</span>
-      </button>
+      {!filterContext?.activeSearchTerm ? (
+        <div className="flex">
+          <input
+            className="focus:shadow-outline w-96 max-w-6xl appearance-none rounded border px-3 py-2 leading-tight text-gray-700 shadow-md focus:outline-none"
+            id="username"
+            type="text"
+            placeholder="Search"
+            value={(filterContext && filterContext.searchTerm) || ""}
+            onChange={(e) =>
+              filterContext && filterContext.setSearchTerm(e.target.value)
+            }
+          />
+          <button
+            className="mx-5 flex items-center rounded px-3 outline outline-cyan-700 hover:scale-105 hover:text-indigo-200 hover:outline-indigo-200"
+            style={{ boxShadow: "none" }}
+            onClick={() => setSearchResults(filterContext?.searchTerm || "")}
+          >
+            <span className="p-1">Search</span>
+            <span className="p-1">{search}</span>
+          </button>
+        </div>
+      ) : (
+        <button
+          className="mx-5 flex items-center justify-center rounded px-3 outline outline-pink-400 hover:scale-105 hover:text-indigo-200 hover:outline-indigo-200"
+          style={{ boxShadow: "none" }}
+          onClick={() => {
+            setSearchResults("");
+            filterContext?.setSearchTerm("");
+          }}
+        >
+          <span className="p-1">
+            Clear Search Term:{" "}
+            <span className="ml-2">{filterContext?.activeSearchTerm}</span>
+          </span>
+          <span className="p-1">{xMark}</span>
+        </button>
+      )}
     </div>
   );
 }
