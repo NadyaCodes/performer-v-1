a723947e3257c3e04a5a7518ab16e6e51b9c2f9b
NadyaCodes <nadyacorscadden@gmail.com>
2023-08-23 17:02:25 -0400

    Deployment prep 1 (#19)
    * make admin check on server side for add program
    
    * remove extras from env example

--- src/pages/add-program.tsx
+++ src/pages/add-program.tsx
@@ -1,17 +1,27 @@
 import AddProgramComponent from "@component/components/AddProgram/AddProgramComponent";
-import { NextPage } from "next";
-import { useSession } from "next-auth/react";
+import { GetServerSideProps, NextPage } from "next";
+import { getSession } from "next-auth/react";
 
-const AddProgram: NextPage = () => {
-  const { data: sessionData } = useSession();
-  const userId = sessionData?.user.id;
-  return (
-    <AddProgramComponent
-      admin={
-        (userId && userId === process.env.NEXT_PUBLIC_ADMIN_USER_ID) || false
-      }
-    />
-  );
+interface AddProgramProps {
+  admin: boolean;
+}
+
+const AddProgram: NextPage<AddProgramProps> = ({ admin }) => {
+  return <AddProgramComponent admin={admin} />;
 };
 
 export default AddProgram;
+
+export const getServerSideProps: GetServerSideProps<AddProgramProps> = async (
+  context
+) => {
+  const session = await getSession(context);
+
+  const admin = session?.user?.id === process.env.ADMIN_USER_ID;
+
+  return {
+    props: {
+      admin,
+    },
+  };
+};
