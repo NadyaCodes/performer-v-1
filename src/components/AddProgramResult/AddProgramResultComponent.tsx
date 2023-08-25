1bcc649a423a08b1222eac0c42e1645324515db3
NadyaCodes <nadyacorscadden@gmail.com>
2023-08-25 13:23:35 -0400

    fix warnings in adding programs

--- src/components/AddProgramResult/AddProgramResultComponent.tsx
+++ src/components/AddProgramResult/AddProgramResultComponent.tsx
@@ -1,3 +1,109 @@
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import type {
+  DisciplineObject,
+  NewProgramSubmission,
+  TypeObject,
+} from "../ProgramFinder/types";
+import SingleProgramResult from "./SingleProgramResult";
+
+export type SingleProgramSubmission = {
+  schoolName: string;
+  city: string;
+  province: string;
+  website: string;
+  discipline: string;
+  type: string;
+  programName?: string;
+  tempId: string;
+};
+
+export default function AddProgramResultComponent() {
+  const router = useRouter();
+  const [dataArray, setDataArray] = useState<SingleProgramSubmission[]>([]);
+  const [currentProgram, setCurrentProgram] = useState<number>(0);
+  const [displayPrograms, setDisplayPrograms] = useState<
+    SingleProgramSubmission[]
+  >(dataArray[0] ? [dataArray[0]] : []);
+
+  useEffect(() => {
+    const fetchData = () => {
+      if (router.query.objectToPassToNextURL) {
+        const queryParam: string = router.query.objectToPassToNextURL as string;
+
+        const data: NewProgramSubmission[] = JSON.parse(
+          decodeURIComponent(queryParam)
+        );
+        const newDataArray: SingleProgramSubmission[] = [];
+        data.forEach((item: NewProgramSubmission) => {
+          const types: (keyof TypeObject)[] = ["ft", "pt"];
+          const programTypes = types.filter((ty) => item.type[ty]);
+          const disciplines: (keyof DisciplineObject)[] = [
+            "act",
+            "sing",
+            "dance",
+            "mt",
+          ];
+          const programDisciplines = disciplines.filter(
+            (disc) => item.discipline[disc]
+          );
+          programTypes.forEach((type) => {
+            programDisciplines.forEach((discipline) => {
+              newDataArray.push({
+                schoolName: item.schoolName,
+                city: item.city,
+                province: item.province,
+                website: item.website,
+                discipline: discipline,
+                type: type,
+                tempId: item.tempId,
+                ...(item.programName && { programName: item.programName }),
+              });
+            });
+          });
+        });
+        setDataArray(newDataArray);
+      }
+    };
+
+    fetchData();
+  }, [router.query.objectToPassToNextURL]);
+
+  useEffect(() => {
+    if (
+      dataArray.length > 0 &&
+      currentProgram >= 0 &&
+      currentProgram < dataArray.length
+    ) {
+      const newProgramSubmission = dataArray[currentProgram];
+      if (newProgramSubmission) {
+        setDisplayPrograms((prevDisplayPrograms) => [
+          ...prevDisplayPrograms,
+          newProgramSubmission,
+        ]);
+      }
+    }
+  }, [dataArray, currentProgram]);
+
+  const programDisplay = displayPrograms?.map((object, index) => {
+    return (
+      <SingleProgramResult
+        schoolObject={object}
+        key={index}
+        setCurrentProgram={setCurrentProgram}
+        currentProgram={currentProgram}
+      />
+    );
+  });
+
+  return (
+    <div className="m-6 flex flex-col items-center justify-center">
+      <h1 className="mb-2 text-4xl font-bold text-gray-900">Results</h1>
+      <div>{displayPrograms && programDisplay}</div>
+    </div>
+  );
+}
+
 // import React, { useState, useEffect } from "react";
 // import { useRouter } from "next/router";
 // import type {
@@ -102,107 +208,3 @@
 //     </div>
 //   );
 // }
-
-import React, { useState, useEffect, useRef } from "react";
-import { useRouter } from "next/router";
-import type {
-  DisciplineObject,
-  NewProgramSubmission,
-  TypeObject,
-} from "../ProgramFinder/types";
-import SingleProgramResult from "./SingleProgramResult";
-
-export type SingleProgramSubmission = {
-  schoolName: string;
-  city: string;
-  province: string;
-  website: string;
-  discipline: string;
-  type: string;
-  programName?: string;
-  tempId: string;
-};
-
-export default function AddProgramResultComponent() {
-  const router = useRouter();
-  const [dataArray, setDataArray] = useState<SingleProgramSubmission[]>([]);
-  const [currentProgram, setCurrentProgram] = useState<number>(0);
-  const [displayPrograms, setDisplayPrograms] = useState<
-    SingleProgramSubmission[]
-  >(dataArray[0] ? [dataArray[0]] : []);
-
-  useEffect(() => {
-    const fetchData = () => {
-      if (router.query.objectToPassToNextURL) {
-        const data: NewProgramSubmission[] = JSON.parse(
-          decodeURIComponent(router.query.objectToPassToNextURL as string)
-        );
-        const newDataArray: SingleProgramSubmission[] = [];
-        data.forEach((item: NewProgramSubmission) => {
-          const types: (keyof TypeObject)[] = ["ft", "pt"];
-          const programTypes = types.filter((ty) => item.type[ty]);
-          const disciplines: (keyof DisciplineObject)[] = [
-            "act",
-            "sing",
-            "dance",
-            "mt",
-          ];
-          const programDisciplines = disciplines.filter(
-            (disc) => item.discipline[disc]
-          );
-          programTypes.forEach((type) => {
-            programDisciplines.forEach((discipline) => {
-              newDataArray.push({
-                schoolName: item.schoolName,
-                city: item.city,
-                province: item.province,
-                website: item.website,
-                discipline: discipline,
-                type: type,
-                tempId: item.tempId,
-                ...(item.programName && { programName: item.programName }),
-              });
-            });
-          });
-        });
-        setDataArray(newDataArray);
-      }
-    };
-
-    fetchData();
-  }, [router.query.objectToPassToNextURL]);
-
-  useEffect(() => {
-    if (
-      dataArray.length > 0 &&
-      currentProgram >= 0 &&
-      currentProgram < dataArray.length
-    ) {
-      const newProgramSubmission = dataArray[currentProgram];
-      if (newProgramSubmission) {
-        setDisplayPrograms((prevDisplayPrograms) => [
-          ...prevDisplayPrograms,
-          newProgramSubmission,
-        ]);
-      }
-    }
-  }, [dataArray, currentProgram]);
-
-  const programDisplay = displayPrograms?.map((object, index) => {
-    return (
-      <SingleProgramResult
-        schoolObject={object}
-        key={index}
-        setCurrentProgram={setCurrentProgram}
-        currentProgram={currentProgram}
-      />
-    );
-  });
-
-  return (
-    <div className="m-6 flex flex-col items-center justify-center">
-      <h1 className="mb-2 text-4xl font-bold text-gray-900">Results</h1>
-      <div>{displayPrograms && programDisplay}</div>
-    </div>
-  );
-}
