328dd43627937b77adbdac56b663c108c01d3c51
NadyaCodes <nadyacorscadden@gmail.com>
2023-09-07 18:27:18 -0400

    make initial blog template

--- src/components/Blog/BlogPageComponent.tsx
+++ src/components/Blog/BlogPageComponent.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import type { Post } from "@prisma/client";
 
 export type BlogProps = {
@@ -7,16 +7,122 @@ export type BlogProps = {
 };
 
 export default function BlogPageComponent({ post, date }: BlogProps) {
+  const [imageTop, setImageTop] = useState<number>(35);
+  const [imageRight, setImageRight] = useState<number>(-20);
   const postArray = post.body.split("\n");
 
-  const paragraphDisplay = postArray.map((element) => <div>{element}</div>);
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const newImageTop = -scrollY * 0.03 + 35;
+      setImageTop(newImageTop);
+
+      const newImageRight = scrollY * 0.06 - 20;
+      setImageRight(newImageRight);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  // const paragraphDisplay = postArray.map((text, index) =>
+  //   text.includes("####") ? (
+  //     <div
+  //       key={index}
+  //       className="my-10 rounded border-t-4 border-cyan-700"
+  //     ></div>
+  //   ) : text.startsWith("#H2#") ? (
+  //     <h2 key={index} className="my-8 pl-3 text-xl font-bold">
+  //       {text.slice(4)}
+  //     </h2>
+  //   ) : text.startsWith("#H3#") ? (
+  //     <h3 key={index} className="text-md my-8 text-center font-bold">
+  //       {text.slice(4)}
+  //     </h3>
+  //   ) : text.startsWith("#UL#") ? (
+  //     const listArray = text.split("#LI#")
+  //     <ul>{text}</ul>
+  //   ) : (
+  //     <div key={index} className="my-3">
+  //       {text}
+  //     </div>
+  //   )
+  // );
+
+  const paragraphDisplay = postArray.map((text, index) => {
+    if (text.includes("####")) {
+      return (
+        <div
+          key={index}
+          className="my-10 rounded border-t-4 border-cyan-700"
+        ></div>
+      );
+    } else if (text.startsWith("#H2#")) {
+      return (
+        <h2 key={index} className="my-8 pl-3 text-xl font-bold">
+          {text.slice(4)}
+        </h2>
+      );
+    } else if (text.startsWith("#H3#")) {
+      return (
+        <h3 key={index} className="text-md my-8 text-center font-bold">
+          {text.slice(4)}
+        </h3>
+      );
+    } else if (text.startsWith("#UL#")) {
+      const listArray = text.split("#LI#");
+      return (
+        <div key={index} className="my-5 font-bold">
+          {listArray[0]?.slice(4)}
+          <ul className="list-disc pl-7 font-normal italic">
+            {listArray.map((item, listItemIndex) =>
+              listItemIndex !== 0 ? <li key={listItemIndex}>{item}</li> : null
+            )}
+          </ul>
+        </div>
+      );
+    } else {
+      return (
+        <div key={index} className="my-3">
+          {text}
+        </div>
+      );
+    }
+  });
 
   return (
-    <div>
-      <div className="text-xl font-bold">{post.title}</div>
+    <div className="m-2 flex flex-col items-center pb-10 text-cyan-900 mobileMenu:m-0">
+      <div
+        className="absolute left-0 right-0 hidden h-10 bg-cyan-900 mobileMenu:block"
+        style={{
+          boxShadow:
+            "inset 0px -1px 2px rgba(0,255,255,0.5), inset 0px -2px 4px rgba(0,255,255,0.5), inset 0px -4px 8px rgba(0,255,255,0.5)",
+        }}
+      ></div>
+      <div
+        className="text-bold flex w-full flex-col content-center items-center p-3 text-center text-lg mobileMenu:mt-10"
+        style={{ animation: "fadeIn .7s linear" }}
+      ></div>
+      <div className="m-2 w-11/12 text-center text-3xl font-bold mobileMenu:w-9/12">
+        {post.title}
+      </div>
       <div className="italic">~{post.author}</div>
-      <div>{date ? date.toLocaleString() : ""}</div>
-      <div>{paragraphDisplay}</div>
+      <div>{date ? date.toDateString() : ""}</div>
+
+      <div className="mt-5 overflow-y-hidden text-cyan-950 mobileMenu:w-10/12">
+        <div className=" relative flex justify-center">
+          <div
+            dangerouslySetInnerHTML={{ __html: post.image }}
+            className="absolute opacity-0 mobileMenu:opacity-20"
+            style={{
+              top: `${imageTop}rem`,
+              right: `${imageRight}rem`,
+            }}
+          />
+        </div>
+        {paragraphDisplay}
+      </div>
     </div>
   );
 }
--- src/components/Blog/BlogPreview.tsx
+++ src/components/Blog/BlogPreview.tsx
@@ -6,9 +6,12 @@ export default function BlogPreview({ postArray }: { postArray: Post[] }) {
   const previewDisplay = postArray.map((element) => {
     return (
       <Link href={`/blog/${element.slug}`}>
-        <div className="flex flex-col">
-          <div className="h-52 w-52">
-            <div dangerouslySetInnerHTML={{ __html: element.image }} />
+        <div className="m-5 flex max-w-md flex-col items-center rounded-lg border border-indigo-200 p-3 shadow-lg shadow-cyan-800 transition-all hover:scale-105">
+          <div className="m-3 h-52 w-52 overflow-hidden rounded-lg border-cyan-600 bg-cyan-800 shadow-md shadow-cyan-900">
+            <div
+              dangerouslySetInnerHTML={{ __html: element.image }}
+              className="flex h-full w-full items-center justify-center"
+            />
           </div>
           <div className="font-bold">{element.title}</div>
         </div>
